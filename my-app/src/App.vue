9587cebe4607926ec6103cb54acd64ffccb9a49b
ycy <568082704@qq.com>
2019-09-10 20:58:35 +0800

    sava

---
+++ my-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <div>
+      <input type="text" v-model="info" />
+      <button @click="handleClick">添加</button>
+      <ul>
+        <todo-item v-for="item in list" :key="item" :item="item"></todo-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem";
+export default {
+  name: "app",
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      msg: "hello geektime",
+      info: "",
+      list: []
+    };
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.info);
+      this.info = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
